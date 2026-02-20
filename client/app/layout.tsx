f6589ebb434c1690a9246a63814849c62a423314
Tobiloba Awolaju <120692959+tobiawolaju@users.noreply.github.com>
2026-02-20 20:14:53 +0100

    :0

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import PrivyProviderWrapper from "@/components/PrivyProviderWrapper";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,9 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <PrivyProviderWrapper>
-          {children}
-        </PrivyProviderWrapper>
+        {children}
       </body>
     </html>
   );
